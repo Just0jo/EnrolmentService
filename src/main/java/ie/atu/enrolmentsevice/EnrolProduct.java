d6a8907902dffa0e3364412c54028df9f0fc8daa
Just0jo <G00407342@atu.ie>
2024-10-21 13:00:29 +0100

    product class populated

--- src/main/java/ie/atu/enrolmentsevice/EnrolProduct.java
+++ src/main/java/ie/atu/enrolmentsevice/EnrolProduct.java
@@ -1,6 +1,25 @@
 package ie.atu.enrolmentsevice;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class EnrolProduct {
+        private long enrollmentId;
+
+        private long studentId;
+
+        private String studentName;
+
+        private String courseCode;
+
+        private String enrolmentDate;
+
+
+    }
+
 
 
 }
